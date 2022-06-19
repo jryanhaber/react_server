f64c9e64cf7201dcf1013f626403944ac7b4601c
Jonathan Haber <jryanhaber@gmail.com>
2022-06-19 15:25:54 -0700

    hotfix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 // pull in all functions from passport refactored to another file
-require('.services/passport');
+require('./services/passport');
 // define the app as a function of express to initialize it with express functions
 const app = express();
 
 // call in the function exported in authroutes and immediately call it with the app variable
-require('.routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
